c76b2702aa543acfbb5429ba71ec3aac9bdf6c94
rob93c <rob.uniud@gmail.com>
2019-06-26 23:12:46 +0200

    Added the Evento class to model Evento objects

---
+++ WebCommunity/src/it/academy_modis/Evento.java
@@ -0,0 +1,41 @@
+package it.academy_modis;
+
+public class Evento {
+	
+	private int id_categoria;
+	private String nome;
+	private String luogo;
+	private String provincia;
+	private String data;
+	
+	public Evento(int id_categoria, String nome, String luogo, String provincia, String data) {
+		super();
+		this.id_categoria = id_categoria;
+		this.nome = nome;
+		this.luogo = luogo;
+		this.provincia = provincia;
+		this.data = data;
+	}
+
+	public int getId_categoria() {
+		return id_categoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getLuogo() {
+		return luogo;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public String getData() {
+		return data;
+	}
+	
+	
+}
